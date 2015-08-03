938300b21ecc2493761dbb0989e5ec9a2fe7252c
mitchellhart <mitchellhart@gmail.com>
2015-08-03 17:45:39 -0400

    adds loop to iterate through next 9 pages of search results

--- app/wrappers/wiki_wrapper.rb
+++ app/wrappers/wiki_wrapper.rb
@@ -12,20 +12,34 @@ class WikiWrapper
     @prop = "prop=revisions"
   end
 
-  def get_json(title) 
+  def get_json(title, options={}) 
     self.titles = "titles=#{title.gsub(" ", "%20")}"
     url = "#{@callback}&#{@format}&#{@action}&#{@prop}&#{@rvlimit}&#{self.titles}&rvdiffto=prev"
+    if options[:rvcontinue] 
+      url += "&rvcontinue=#{options[:rvcontinue]}"
+    end
     html = open(url)
     JSON.load(html)
   end
 
   def get_page(title)
     json = get_json(title)
+    rvcontinue = json["continue"]["rvcontinue"]
     page_id = json["query"]["pages"].keys.first
     query = json["query"]["pages"][page_id]
     page = Page.new(title: query["title"])
     revisions = query["revisions"]
     add_revisions_to_page(page, revisions)
+
+    9.times do |i|
+      json = get_json(title, {rvcontinue: rvcontinue})
+      rvcontinue = json["continue"]["rvcontinue"]
+      page_id = json["query"]["pages"].keys.first
+      query = json["query"]["pages"][page_id]
+      page = Page.new(title: query["title"])
+      revisions = query["revisions"]
+      add_revisions_to_page(page, revisions)
+    end
   end
 
   def add_revisions_to_page(page, revisions)
@@ -34,7 +48,7 @@ class WikiWrapper
       timestamp = r['timestamp']
       comment = r['comment']
       content = r['diff']['*']
-      revision = Revision.new(timestamp: timestamp, comment: comment, content: content)
+      revision = Revision.new(time: timestamp, content: content)
       revision.save
       page.revisions << revision
     end
