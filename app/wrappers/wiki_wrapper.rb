714f429a87d05d764b85505de081b178497ba793
Becckitt <hello@beccabarton.com>
2015-08-03 16:19:18 -0400

    adds get_page method

--- app/wrappers/wiki_wrapper.rb
+++ app/wrappers/wiki_wrapper.rb
@@ -1,12 +1,22 @@
 class WikiWrapper 
-  attr_accessor :callback, :format, :action, :prop, :pclimit, :titles
+  require 'json'
+  require 'open-uri'
+
+  attr_accessor :callback, :format, :action, :prop, :rvlimit, :titles
 
   def initialize
     @callback = "https://en.wikipedia.org/w/api.php?"
-    @format = "json"
-    @action = "query"
-    @pclimit = "500"
+    @format = "format=json"
+    @action = "action=query"
+    @rvlimit = "rvlimit=500"
+    @prop = "prop=revisions"
   end
 
-  
+  def get_page(title) 
+    self.titles = "titles=#{title.gsub(" ", "%20")}"
+    url = "#{@callback}&#{@format}&#{@action}&#{@prop}&#{@rvlimit}&#{self.titles}"
+    html = open(url)
+    json = JSON.load(html)
+    
+  end
 end
