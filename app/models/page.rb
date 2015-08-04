8866dae6ac2c3df0cbb38440caa20258f6282fa4
apradhn <abhijit.pradhan6@gmail.com>
2015-08-04 14:58:31 -0400

    modifies #top_five_authors to return array of hashes, containing object and revision count

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,7 +3,10 @@ class Page < ActiveRecord::Base
   has_many :authors, :through => :revisions
 
   def top_five_authors
-    self.authors.group(:name).order('count_id desc').count('id').max_by(5){|name, num| num}
+    results = self.authors.group(:name).order('count_id desc').count('id').max_by(5){|name, num| num}
+    results.collect do |r|
+      {author: Author.find_by(name: r[0]), count: r[1]}
+    end
   end
 
   
