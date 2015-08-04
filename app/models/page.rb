0c9b2f6fa90e4b4efde4de07466574ff48190523
Becckitt <hello@beccabarton.com>
2015-08-04 16:40:37 -0400

    Adds find author by name method, adds to show for Page

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,8 +3,13 @@ class Page < ActiveRecord::Base
   has_many :authors, :through => :revisions
 
   def top_five_authors
-    self.authors.group(:name).order('count_id desc').count('id').max_by(5){|name, num| num}
+    self.authors.group(:name).order('count_id desc').count('id').max_by(5){|id, num| num}
   end
+
+  def find_author_by_name(author_name)
+    self.authors.find_by(name: author_name)
+  end
+
 
   
 end
