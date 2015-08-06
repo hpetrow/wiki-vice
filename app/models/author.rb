a46c13384d26623bdb053976043ad3570c299e8e
apradhn <abhijit.pradhan6@gmail.com>
2015-08-06 12:36:44 -0400

    improves load speed of author show page by fetching all user contribs only if author has one unique page

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,12 +6,16 @@ class Author < ActiveRecord::Base
   def top_contributions
     # Returns the pages that the author has contributed to the most
     # and the amount of contributions he has made to that page
-    WIKI.get_user_contributions(self) if self.pages == 1
+    WIKI.get_user_contributions(self) if self.unique_pages.size == 1
     self.pages.group(:title).order('count_id desc').count('id').max_by(5){|name, num| num}
   end
 
   def most_recent_revision
     self.revisions.order("timestamp desc").limit(1).first
+  end
+
+  def unique_pages
+    self.pages.uniq
   end
 
 
