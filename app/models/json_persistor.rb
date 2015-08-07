ba6fbfce4a78eb33910943fc58bca34841996cc6
apradhn <abhijit.pradhan6@gmail.com>
2015-08-07 00:07:35 -0400

    renames name to title on categories

--- app/models/json_persistor.rb
+++ app/models/json_persistor.rb
@@ -8,12 +8,19 @@ class JsonPersistor
   def persist_page_revisions
     page_id = json["query"]["pages"].keys.first
     page_data = json["query"]["pages"][page_id]
-    binding.pry
     page = Page.new(title: page_data["title"], page_id: page_id)
     if page.save
+      persist_categories(page)
     else
       Page.find_by(page_id: page_id)
     end
   end
 
+  def persist_categories(page)
+    categories = json["query"]["pages"][page.page_id.to_s]["categories"]
+    all_categories = Category.pluck(:name)
+    categories.each do |category|
+
+    end
+  end
 end
