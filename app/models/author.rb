805df9492daf81a0894223b979324af8dd6f69c9
apradhn <abhijit.pradhan6@gmail.com>
2015-08-09 19:25:08 -0400

    fixes bug on author model to reflect changes to wiki wrapper

--- app/models/author.rb
+++ app/models/author.rb
@@ -13,7 +13,7 @@ class Author < ActiveRecord::Base
   def most_recent_revision
     revision = self.revisions.order("timestamp desc").limit(1).first
     if revision.content.nil?
-      WIKI.get_content_for_revision(revision)
+      WIKI.get_revision_content(revision)
     end
     revision
   end
