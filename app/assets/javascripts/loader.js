df8ff019a6d85e817d686f40e32a5f20fe98f63b
Becckitt <hello@beccabarton.com>
2015-08-12 14:31:00 -0400

    adds fully functioning *i think* loading screen. going to see if there's a better way to do this though.

--- app/assets/javascripts/loader.js
+++ app/assets/javascripts/loader.js
@@ -1,11 +1,10 @@
-$(function() {
-  var button = $('#search-loader');
-  var loadPopUp = $('.loader');
-  var pageDimmer = $('#page-dimmer');
-  
-  button.click(function() {
-    loadPopUp.show();
-    pageDimmer.show();
-  });
+ $(function() {
+    var button = $('#search-loader');
+      var loadPopUp = $('.loader');
+      var hideOnSubmit = $('.hideOnSubmit');
 
-});
+      button.click(function() {
+        loadPopUp.fadeIn();
+        hideOnSubmit.fadeOut();
+      });
+ });
