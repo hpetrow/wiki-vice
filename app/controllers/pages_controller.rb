911ac8e06715ea68e54a95e121f94ae827c0592d
apradhn <abhijit.pradhan6@gmail.com>
2015-08-16 18:37:46 -0400

    merges wiki worker

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,16 +19,17 @@ class PagesController < ApplicationController
 
   def show
     @page = Page.find(params[:id])
-    if @page.revisions.size < 10
-      wiki = WikiWrapper.new
-      @page = wiki.get_page(@page.title)
-    end
-    @page.new_vandalism
-    gon.revDates = @page.format_rev_dates_for_c3
-    gon.revCounts = @page.format_rev_counts_for_c3
-    gon.anonLocationMap = @page.anonymous_location_for_map
-    gon.extractTitle = @page.title
-    gon.extractPageId = @page.page_id
+    @job = WikiWorker.perform_async(params[:id])
+    # if @page.revisions.size < 10
+    #   wiki = WikiWrapper.new
+    #   @page = wiki.get_page(@page.title)
+    # end
+    # @page.new_vandalism
+    # gon.revDates = @page.format_rev_dates_for_c3
+    # gon.revCounts = @page.format_rev_counts_for_c3
+    # gon.anonLocationMap = @page.anonymous_location_for_map
+    # gon.extractTitle = @page.title
+    # gon.extractPageId = @page.page_id
   end
 
   def random
