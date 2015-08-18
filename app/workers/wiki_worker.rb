79dccfa56d0e28add477ca17349215e5e35e3599
apradhn <abhijit.pradhan6@gmail.com>
2015-08-17 22:46:41 -0400

    precompiles assets

--- app/workers/wiki_worker.rb
+++ app/workers/wiki_worker.rb
@@ -14,7 +14,6 @@ class WikiWorker
       secret: '6e55dd3d3001f6ed63f7'
     }
     ) 
-    sleep(5)
     pusher.trigger("page_results_#{page.id}", "get_page", {:id => page.id, :title => page.title, :revisionRate => page.time_between_revisions})
   end
 end
