cafd4efe5874c4e90066d52fbc7407a75f3d6e71
Flatiron School <hello@beccabarton.com>
2015-11-04 01:44:15 -0500

    adds first page in haml /

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,8 +1,9 @@
 class AuthorsController < ApplicationController
+
   def show
     @author = Author.find(params[:id])
     @top_contributions = @author.top_contributions
-    @revision_parser = RevisionParser.new
+    @revision_parser = RevisionParser.new 
     gon.data = @top_contributions
     # @comment_info = @author.get_user_contributions_info
   end
