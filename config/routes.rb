137cc37ece55b8698cc10b3af08bb6b7c4c09323
Becckitt <hello@beccabarton.com>
2015-11-11 17:02:08 -0500

    adds readme text to an actual, real about page. finally!

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  get 'about' => 'welcome#about'
   get 'random' => 'pages#random'
   get 'dashboard' => "pages#dashboard"
   get 'map' => 'pages#map'
